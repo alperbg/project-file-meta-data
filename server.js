60512bd5cae6f5286ddceddd109f91a62c0803c8
4ppe <alper0524@gmail.com>
2021-02-20 06:38:41 +0300

    upload route with multer

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+
 require('dotenv').config()
 
 var app = express();
@@ -7,12 +9,27 @@ var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+
+const upload = multer();
+
+//Upload route
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  try {
+    return res.status(201).json({
+      message: 'File uploded successfully',
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 
-
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
